d38dc64da0263c12040787f21ef4335a1e41f9bf
Nathan Parikh <nathan.parikh@duke.edu>
2020-08-24 10:05:13 -0700

    user can enter HDL test result

--- calculator.py
+++ calculator.py
@@ -1,10 +1,29 @@
 def interface():
     print("My Program")
-    print("Options:")
-    print("9 - Quit")
-    choice = input("Enter your choice: ")
-    if choice=='9':
-        return
+    while true:
+	    print("Options:")
+	    print ("1 - HDL")
+	    print("9 - Quit")
+	    choice = input("Enter your choice: ")
+	    if choice=='9':
+	        return
+	    elif choice == '1':
+	    	HDL_driver()
    
 interface()
+
+def HDL_drive():
+	hdl_input = HDL_input()
+	#Check if HDL is normal
+	#Output
+
+
+def HDL_input():
+	hdl_input = input("Enter your HDL test result: ")
+	return int(hdl_input)
+
+def HDL_normal():
+
+
+def HDL_output():
 
